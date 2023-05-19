63d87212ce041c4b01f00021b69d49fcf547200d
Selin Uygun <59924224+su519@users.noreply.github.com>
2023-05-19 19:57:48 +0100

    Multiplexer binary read

--- Arduino/multiplexer_test/multiplexer_test.ino
+++ Arduino/multiplexer_test/multiplexer_test.ino
@@ -5,103 +5,44 @@
 #define i2 6
 
 int count = 0;
+
 void setup() {
   pinMode(A, OUTPUT);
   pinMode(B, OUTPUT);
   pinMode(C, OUTPUT);
   pinMode(i1, INPUT);
   pinMode(i2, INPUT);
-//  digitalWrite(vapo,HIGH);
 
   Serial.begin(9600);
-  
 }
 
 void loop() {
-  if(count == 0){
-    digitalWrite(A,0);
-    digitalWrite(B,0);
-    digitalWrite(C,0);
-    Serial.print(digitalRead(i1));
-    delay(200);
-    digitalWrite(A,1);
-    digitalWrite(B,0);
-    digitalWrite(C,0);
-    Serial.print(digitalRead(i1));
-    delay(200);
-    digitalWrite(A,0);
-    digitalWrite(B,1);
-    digitalWrite(C,0);
-    Serial.print(digitalRead(i1));
-    delay(200);
-    digitalWrite(A,1);
-    digitalWrite(B,1);
-    digitalWrite(C,0);
-    Serial.print(digitalRead(i1));
-    delay(200);
-    digitalWrite(A,0);
-    digitalWrite(B,0);
-    digitalWrite(C,1);
-    Serial.print(digitalRead(i1));
-    delay(200);
-    digitalWrite(A,1);
-    digitalWrite(B,0);
-    digitalWrite(C,1);
-    Serial.print(digitalRead(i1));
-    delay(200);
-    digitalWrite(A,0);
-    digitalWrite(B,1);
-    digitalWrite(C,1);
-    Serial.print(digitalRead(i1));
-    delay(200);
-    digitalWrite(A,1);
-    digitalWrite(B,1);
-    digitalWrite(C,1);
-    Serial.print(digitalRead(i1));
-    delay(200);   
-    
-    digitalWrite(A,0);
-    digitalWrite(B,0);
-    digitalWrite(C,0);
-    Serial.print(digitalRead(i2));
-    delay(200);
-    digitalWrite(A,1);
-    digitalWrite(B,0);
-    digitalWrite(C,0);
-    Serial.print(digitalRead(i2));
-    delay(200);
-    digitalWrite(A,0);
-    digitalWrite(B,1);
-    digitalWrite(C,0);
-    Serial.print(digitalRead(i2));
-    delay(200);
-    digitalWrite(A,1);
-    digitalWrite(B,1);
-    digitalWrite(C,0);
-    Serial.print(digitalRead(i2));
-    delay(200);
-    digitalWrite(A,0);
-    digitalWrite(B,0);
-    digitalWrite(C,1);
-    Serial.print(digitalRead(i2));
-    delay(200);
-    digitalWrite(A,1);
-    digitalWrite(B,0);
-    digitalWrite(C,1);
-    Serial.print(digitalRead(i2));
-    delay(200);
-    digitalWrite(A,0);
-    digitalWrite(B,1);
-    digitalWrite(C,1);
-    Serial.print(digitalRead(i2));
-    delay(200);
-    digitalWrite(A,1);
-    digitalWrite(B,1);
-    digitalWrite(C,1);
-    Serial.print(digitalRead(i2));
-    delay(200);
+  if (count == 0) {
+
+    int binary = 0;
+
+    for (int add = 0; add <= 7; add = add + 1) {
+      digitalWrite(A, (add & 0b001) >> 0);
+      digitalWrite(B, (add & 0b010) >> 1);
+      digitalWrite(C, (add & 0b100) >> 2);
+
+      binary = (binary << 1) | digitalRead(i2);
+      delay(1);
+    }
+
+    for (int add = 0; add <= 7; add = add + 1) {
+      digitalWrite(A, (add & 0b001) >> 0);
+      digitalWrite(B, (add & 0b010) >> 1);
+      digitalWrite(C, (add & 0b100) >> 2);
+
+      binary = (binary << 1) | digitalRead(i1);
+      delay(1);
+    }
+
+    for (int i = 15; i >= 0; i = i - 1) {
+      Serial.print((binary >> i) & 0x01);
+    }
+    Serial.println();
     count = 1;
   }
-
-  
 }
