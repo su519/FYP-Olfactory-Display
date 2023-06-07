808f8971e1de4ecfdf7bdd243e30844c63ee9ee6
Selin Uygun <59924224+su519@users.noreply.github.com>
2023-06-07 12:04:44 +0100

    3d

--- Arduino/multiplexer_test/multiplexer_test.ino
+++ Arduino/multiplexer_test/multiplexer_test.ino
@@ -1,8 +1,9 @@
-#define A 8
-#define B 9
-#define C 10
-#define i1 7
-#define i2 6
+#define A 11
+#define B 12
+#define C 13
+#define i1 8
+#define i2 9
+
 
 int count = 0;
 
@@ -12,37 +13,39 @@ void setup() {
   pinMode(C, OUTPUT);
   pinMode(i1, INPUT);
   pinMode(i2, INPUT);
-
+  pinMode(17, OUTPUT);
+  digitalWrite(17, LOW);
   Serial.begin(9600);
+
+
 }
 
 void loop() {
-  if (count == 0) {
+  unsigned long binary = 0;
 
-    unsigned long binary = 0;
-
-    for (int add = 0; add <= 7; add = add + 1) {
-      digitalWrite(A, (add & 0b001) >> 0);
-      digitalWrite(B, (add & 0b010) >> 1);
-      digitalWrite(C, (add & 0b100) >> 2);
-
-      binary = (binary << 1) | digitalRead(i2);
-      delay(1);
-    }
-
-    for (int add = 0; add <= 7; add = add + 1) {
-      digitalWrite(A, (add & 0b001) >> 0);
-      digitalWrite(B, (add & 0b010) >> 1);
-      digitalWrite(C, (add & 0b100) >> 2);
-
-      binary = (binary << 1) | digitalRead(i1);
-      delay(1);
-    }
-
-    for (int i = 15; i >= 0; i = i - 1) {
-      Serial.print((binary >> i) & 0x01);
-    }
-    Serial.println();
-    count = 1;
+  for (int add = 0; add <= 7; add = add + 1) {
+    digitalWrite(A, (add & 0b001) >> 0);
+    digitalWrite(B, (add & 0b010) >> 1);
+    digitalWrite(C, (add & 0b100) >> 2);
+    
+  
+    binary = (binary << 1) | digitalRead(i2);
+    delay(1);
+    
   }
+  
+  for (int add = 0; add <= 7; add = add + 1) {
+    digitalWrite(A, (add & 0b001) >> 0);
+    digitalWrite(B, (add & 0b010) >> 1);
+    digitalWrite(C, (add & 0b100) >> 2);
+  
+    binary = (binary << 1) | digitalRead(i1);
+    delay(1);
+  }
+  
+  for (int i = 15; i >= 0; i = i - 1) {
+    Serial.print((binary >> i) & 0x01);
+  }
+  Serial.println();
+
 }
