433d0f625a2b2a0375f9fc2b1fd69431ea62f870
Selin Uygun <59924224+su519@users.noreply.github.com>
2023-06-19 11:13:33 +0100

    photos

--- Test/Analysis/test1.m
+++ Test/Analysis/test1.m
@@ -1,7 +1,7 @@
 close all
+
+C1 = confusionmat(t1(:,1),t1(:,2));
+label = {'Lemon','Lavender','Orange','Iris','Lemongrass','Sakura'};
 figure
-
-C1 = confusionmat(test1_equal(:,1),test1_equal(:,2));
-
 cm = confusionchart(C1,label,'RowSummary','row-normalized')
 cm.Title = "Scent Identification Confusion Matrix"
